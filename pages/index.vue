131fee89bfd5c3b0ad6f0159a326866eeb389f9c
Roman Skrypnik <r.skrypnik@yual.io>
2023-08-14 14:07:42 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    index page
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'test',
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
